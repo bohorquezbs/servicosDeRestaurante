309d1a61229a3ed416cfc691fb68ba40fdcc3976
Moises Cruz <serviciopatrimonial@gmail.com>
2025-02-03 10:36:16 -0600

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex flex-col w-screen h-screen bg-[#DA5E2B] items-center pt-3'>
+        <h1 className='text-[#515151] font-extrabold text-2xl'>Las Delicias de la Cena</h1>
+        <input type="text" className='h-10 w-70 p-2 border border-[#ECECEC] bg-[#ECECEC] rounded-lg' />
+        <div className='flex flex-col justify-items-start h-full w-full bg-[#ECECEC] mb-6 mt-4 p-7 gap-6'>
+          <div className='flex items-center'>
+            <h3 className='text-[#875638] text-xl font-bold rotate-270 h-4'>Tortas</h3>
+            <div>
+              <img src="../public/ImagesFood/Totas.jpg" alt="" className='flex flex-col h-32 w-60' />
+              <h3 className='text-[#875638] font-bold'>Precio</h3>
+            </div>
+          </div>
+          <div className='flex items-center'>
+            <h3 className='text-[#875638] text-xl font-bold rotate-270 h-4'>Tacos</h3>
+            <div>
+              <img src="../public/ImagesFood/Tacos.png" alt="" className='flex flex-col h-32 w-60' />
+              <h3 className='text-[#875638] font-bold'>Precio</h3>
+            </div>
+          </div>
+          <div className='flex items-center'>
+            <h3 className='text-[#875638] text-xl font-bold rotate-270 h-4'>Quesadilla</h3>
+            <div>
+              <img src="../public/ImagesFood/Quesadilla.jpeg" alt="" className='flex flex-col h-32 w-60' />
+              <h3 className='text-[#875638] font-bold'>Precio</h3>
+            </div>
+          </div>
+          <div className='flex items-center'>
+            <h3 className='text-[#875638] text-xl font-bold rotate-270 h-4'>Sopes</h3>
+            <div>
+              <img src="../public/ImagesFood/Sopes.jpg" alt="" className='flex flex-col h-32 w-60' />
+              <h3 className='text-[#875638] font-bold'>Precio</h3>
+            </div>
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card bg-amber-100">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+
   )
 }
-
-export default App
