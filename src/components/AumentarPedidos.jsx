2b7fadf785d6d27ce6a5ece301ea3d6c9ecf6fd7
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-03 08:30:55 -0600

    commit

---
+++ src/components/AumentarPedidos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AumentarPedidos() {
+  return (
+    <div>
+        <div>
+            <input type="text" />
+        </div>
+    </div>
+  )
+}
