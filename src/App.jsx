7cce906c3bbb93b93a8cb7c7e9d6b53901b63a53
bohorquezbs <bsbm121@gmail.com>
2025-02-04 10:57:21 -0500

    detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router"
 import { Bebidas } from "./pages/Bebidas"
 import { useState } from "react"
+import { Detail } from "./components/Detail"
 
 function App() {
   const [items, setItems] = useState(0)
@@ -14,7 +15,15 @@ function App() {
             setItems={setItems}
           />
           } />
+          <Route 
+          path="/detalle"
+          element={<Detail
+          items={items}
+          setItems={setItems}
+          />}
+          />
       </Routes>
+      
 
     </>
   )
