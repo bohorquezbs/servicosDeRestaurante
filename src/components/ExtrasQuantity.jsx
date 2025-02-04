c0e9410ea7f4c98e68979908ed15d043f19f8bdf
@Omner3 <gonzalezomner@hotmail.com>
2025-02-04 09:38:52 -0600

    función contar

--- src/components/ExtrasQuantity.jsx
+++ src/components/ExtrasQuantity.jsx
@@ -1,45 +1,64 @@
+import React, { useState } from 'react';
+
 export function ExtrasQuantity() {
+
+  const data = [
+    {
+      id: 1,
+      nombre: "Tomate"
+    },
+    {
+      id: 2,
+      nombre: "Cebolla"
+    },
+    {
+      id: 3,
+      nombre: "Salsa picante"
+    },
+  ]
+
+  const [contador, setContador] = useState({})
+
+  function onIncrease(id) {
+    setContador(prevcontador => ({
+      ...prevcontador,
+      [id]: (prevcontador [id || 0]) +1
+    }))
+  }
+  function onDecrease(id) {
+    setContador(prevcontador => ({
+      ...prevcontador,
+      [id]: Math.max(0, (prevcontador [id] || 0) -1) 
+    }))
+  }
   return (
     <div className="flex flex-col items-center ">
       <nav className="mb-10 w-screen py-5 bg-[#da5e2b]">
         <h1 className="text-center text-white font-semibold text-2xl">Extras</h1>
       </nav>
-      <ul className="self-start mb-60 px-10 ">
-        <li className="flex items-center gap-44">
-          <span>Tomate</span>
-          <div>
-            <button className="font-extrabold rounded-full border text-2xl px-5 py-2.5 text-center mb-3 bg-gray-200 hover:bg-gray-300">
+      <ul className="self-start mb-60 px-10">
+        {data.map(extra => (
+        <li className="flex items-center justify-between space-x-36">
+          <span>{extra.nombre}</span>
+          <div className="flex items-center gap-2">
+            <button
+              onClick={() => onDecrease (extra.id)}
+              className="font-extrabold rounded-full border text-2xl px-3 py-0.5 text-center mb-3 bg-gray-200 hover:bg-gray-300">
               -</button>
-            <span>0</span>
-            <button className="font-extrabold rounded-full border text-2xl px-4 py-2.5 text-center mb-3 bg-gray-200 hover:bg-gray-300">
+            <span>{contador[extra.id] || 0}</span>
+            <button
+              onClick={() => onIncrease (extra.id)}
+              className=" font-extrabold rounded-full border text-2xl px-2 py-0.5 text-center mb-3 bg-gray-200 hover:bg-gray-300">
               +</button>
           </div>
         </li>
-        <li className="flex items-center gap-44">
-          <span>Cebolla</span>
-          <div>
-            <button className="font-extrabold rounded-full border text-2xl px-5 py-2.5 text-center mb-3 bg-gray-200 hover:bg-gray-300">
-              -</button>
-            <span>0</span>
-            <button className="font-extrabold rounded-full border text-2xl px-4 py-2.5 text-center bg-gray-200 hover:bg-gray-300">
-              +</button>
-          </div>
-        </li>
-        <li className="flex items-center gap-32">
-          <span>Salsa picante</span>
-          <div>
-            <button className="font-extrabold rounded-full border text-2xl px-5 py-2.5 text-center bg-gray-200 hover:bg-gray-300">
-              -</button>
-            <span>0</span>
-            <button className="font-extrabold rounded-full border text-2xl px-4 py-2.5 text-center bg-gray-200 hover:bg-gray-300">
-              +</button>
-          </div>
-        </li>
+       
+        ))}
       </ul>
 
-      <ul className="flex gap-20">
-        <button className="border px-5">Cancelar</button>
-        <button className="border px-5 bg-[#e79406]">Confirmar</button>
+      <ul className="flex justify-between space-x-10">
+        <button className="border px-5 hover:bg-gray-300">Cancelar</button>
+        <button className="border px-5 bg-[#e79406] hover:bg-[#b87c04]">Confirmar</button>
       </ul>
     </div>
   )
