78699d9dfff48e721d429b611d4587c91eb5a5f3
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-05 09:02:10 -0600

    solucion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { Route, Routes } from "react-router";
-import { Pedidos } from "./pages/Pedidos";
-import { useState } from "react";
-
 function App() {
-  const [items, setItems] = useState(0);
+
   return (
     <>
-      <Routes>
-        <Route
-          path="/pedidos"
-          element={<Pedidos items={items} setItems={setItems} />}
-        />
-      </Routes>
+     
     </>
   );
 }
